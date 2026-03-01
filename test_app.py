85078b738d386935b072952ea3959b565faeb1f9
Kurkuma <124262649+Jumby191@users.noreply.github.com>
2026-03-01 18:10:28 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,5 +4,4 @@ def test_success():
     assert add(5, 5) == 10
 
 def test_failure():
-    # Мы специально сделаем ошибку позже, а пока напишем правду
-    assert add(1, 1) == 2
+    assert add(1, 1) == 3
